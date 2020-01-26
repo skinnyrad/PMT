95f4ff540f19ebbce5a2439e6a1d06cca0d404b5
David <dmtougaw@gmail.com>
2020-01-26 09:09:14 -0600

    redirect parsing

--- uPy/splash_portal_test/reqst.py
+++ uPy/splash_portal_test/reqst.py
@@ -37,8 +37,8 @@ class Response:
         import ujson
         return ujson.loads(self.content)
 
-
-def request(method, url, data=None, json=None, headers={}, stream=None, timeout=0.5):
+def redirect(method, url, data=None, json=None, headers={}, stream=None, timeout=0.5, parseSplashPage=True):
+    
     try:
         proto, dummy, host, path = url.split("/", 3)
     except ValueError:
@@ -95,6 +95,7 @@ def request(method, url, data=None, json=None, headers={}, stream=None, timeout=
         if len(l) > 2:
             reason = l[2].rstrip()
         while True:
+            # splash page
             l = s.readline()
             if not l or l == b"\r\n":
                 break
@@ -103,11 +104,117 @@ def request(method, url, data=None, json=None, headers={}, stream=None, timeout=
                 if b"chunked" in l:
                     raise ValueError("Unsupported " + l)
             elif l.startswith(b"Location:") and not 200 <= status <= 299:
+                raise OSError("Redirect of Redirect not allowed")
+            #     location = str(l[10:])[2:-5]
+            #     print ("\n\n\n"+location+"\n\n\n")
+            #     # need to get the method from the redirection
+            #     return request('GET',location)
+    except OSError:
+        s.close()
+        raise
+
+    if parseSplashPage:
+        # parse socket bytes
+        founds = []
+        _TAG = b'<a'
+        _temp = b''
+        start = False
+        try:
+        # go through every byte
+            while True:
+                b = s.read(1)
+                if b == b'>':
+                    _temp += b
+                    founds.append(_temp)
+                    _temp = b''
+                    start = False
+                    # startTag found
+                elif len(_TAG) == len(_temp) and (_temp == _TAG):
+                    start = True
+                elif len(_temp) > len(_TAG) and not start:
+                    _temp = b''
+                elif start:
+                    _temp += b
+        except OSError:
+            s.close()
+
+        return founds
+    else:
+        resp = Response(s)
+        resp.status_code = status
+        resp.reason = reason
+        return resp
+
+def request(method, url, data=None, json=None, headers={}, stream=None, timeout=0.5):
+    
+    try:
+        proto, dummy, host, path = url.split("/", 3)
+    except ValueError:
+        proto, dummy, host = url.split("/", 2)
+        path = ""
+    if proto == "http:":
+        port = 80
+    elif proto == "https:":
+        import ussl
+        port = 443
+    else:
+        raise ValueError("Unsupported protocol: " + proto)
+
+    if ":" in host:
+        host, port = host.split(":", 1)
+        port = int(port)
+
+    ai = usocket.getaddrinfo(host, port, 0, usocket.SOCK_STREAM)
+
+    ai = ai[0]
+
+    s = usocket.socket(ai[0], ai[1], ai[2])
+    # set timeout
+    s.settimeout(timeout)
+    try:
+        s.connect(ai[-1])
+        if proto == "https:":
+            s = ussl.wrap_socket(s, server_hostname=host)
+        s.write(b"%s /%s HTTP/1.0\r\n" % (method, path))
+        if not "Host" in headers:
+            s.write(b"Host: %s\r\n" % host)
+        # Iterate over keys to avoid tuple alloc
+        for k in headers:
+            s.write(k)
+            s.write(b": ")
+            s.write(headers[k])
+            s.write(b"\r\n")
+        if json is not None:
+            assert data is None
+            import ujson
+            data = ujson.dumps(json)
+            s.write(b"Content-Type: application/json\r\n")
+        if data:
+            s.write(b"Content-Length: %d\r\n" % len(data))
+        s.write(b"\r\n")
+        if data:
+            s.write(data)
+
+        l = s.readline()
+        #print(l)
+        l = l.split(None, 2)
+        status = int(l[1])
+        reason = ""
+        if len(l) > 2:
+            reason = l[2].rstrip()
+        while True:
+            l = s.readline()
+            if not l or l == b"\r\n":
+                break
+            #print(l)
+            if l.startswith(b"Transfer-Encoding:"):
+                if b"chunked" in l:
+                    raise ValueError("Unsupported " + l)
+            elif l.startswith(b"Location:") and not 200 <= status <= 299:
                 location = str(l[10:])[2:-5]
-                print ("\n\n\n"+location+"\n\n\n")
+                #print ("\n\n\n"+location+"\n\n\n")
                 # need to get the method from the redirection
-                return request('GET',location)
-                # throw NotImplementedError("Redirects not yet supported")
+                return redirect('GET',location)
     except OSError:
         s.close()
         raise
@@ -135,3 +242,27 @@ def patch(url, **kw):
 
 def delete(url, **kw):
     return request("DELETE", url, **kw)
+
+# parser byte by byte
+def parse(r):
+    founds = []
+    _TAG = "<a"
+    _temp = ""
+    start = False
+    # go through every byte
+    for i in range(len(r)):
+        if r[i:i+1] == ">":
+            _temp += r[i:i+1]
+            founds.append(_temp)
+            _temp = ""
+            start = False
+            # startTag found
+        elif len(_TAG) == len(_temp) and (_temp == _TAG):
+            start = True
+        elif len(_temp) > len(_TAG) and not start:
+            _temp = ""
+        elif start:
+            _temp += r[i:i+1]
+
+    return founds
+
---
+++ uPy/splash_portal_test/socket.py
@@ -0,0 +1,83 @@
+# ---------------------------------------
+#  _____  __  __ _______        __   ___  
+# |  __ \|  \/  |__   __|      /_ | / _ \ 
+# | |__) | \  / |  | |    __   _| || | | |
+# |  ___/| |\/| |  | |    \ \ / / || | | |
+# | |    | |  | |  | |     \ V /| || |_| |
+# |_|    |_|  |_|  |_|      \_/ |_(_)___/ 
+# ----------------------------------------
+#  Version 1.0
+#  microPython Firmware esp32spiram-idf3-20191220-v1.12
+#  Filename : main.py
+
+import network
+import machine
+#import urequests
+import usocket
+import ujson
+import utime
+import ubinascii
+import ssl
+
+import reqst
+
+target_host = "www.google.com"
+target_port = 80
+# Create a station object to store our connection
+station = network.WLAN(network.STA_IF)
+
+# activate station
+station.active(True)
+station.scan()
+station.connect("Dunkin' Donuts Guest","")
+while not station.isconnected():
+    utime.sleep(1)
+
+# # Create STREAM TCP socket
+# sock = usocket.socket(usocket.AF_INET,usocket.SOCK_STREAM)
+
+# # time in seconds
+# sock.settimeout(0.5)
+
+# # address resolving
+# sockaddr = usocket.getaddrinfo(targetgit _host, target_port)
+
+# path = '/'
+# # send some data 
+# startRequest = "GET %s HTTP/1.1\r\nHost:%s\r\n\r\n" % (path ,target_host)
+# try:
+#     sock.connect(sockaddr[0][-1])
+#     utime.sleep(1)
+#     sock.send(startRequest.encode()) 
+#     # receive some data 
+#     response = sock.recv(4096)  
+#     http_response = repr(response)
+
+#     httpHeaders = http_response.split('\\r\\n')
+#     url = ""
+#     # find redirect url
+#     for h in httpHeaders:
+#         if h[0:8] == "Location":
+#             #Location url found
+#             url = h.split('//')
+#             break
+#     if url:
+#         urlArray = url.split('/',3)
+#         host = urlArray[0]
+#         sockaddr = usocket.getaddrinfo(host, 80)
+#         sock.connect(sockaddr[0][-1])
+#         utime.sleep(1)
+#         sock.send(startRequest.encode()) 
+#         # receive some data 
+#         response = sock.recv(4096)  
+#         http_response = repr(response)
+
+#         target_host = urlArray[2]
+#         path = urlArray[3]
+#         req = "GET /%s HTTP/1.1\r\nHost:%s\r\n\r\n" % (path ,target_host)
+    
+    
+#     print(str(http_response[0:14]))
+# except OSError as e:
+#     print(e)
+    
