df82ba200b75297f32635f78bd6cd18aa0724c4e
David Tougaw <42074677+DavidTou@users.noreply.github.com>
2020-03-10 14:03:53 -0500

    Update post.py

--- uPy/post.py
+++ uPy/post.py
@@ -27,10 +27,11 @@ def post_data(post_data, post_url, logger: Logger) -> bool:
     try:
         # init harware timer
         timer = Timer(0)
+        #TODO: Uncomment this for solution
         #timer.init(period=3000, mode=Timer.ONE_SHOT,callback=handlerTimer)
         response = post(post_url, headers=headers, data=post_data)
+        #TODO: Uncomment this for solution
         #timer.deinit()
-        #print("Data sent.")
         if response.status_code == 200:
             #TODO: remove print
             response.close()
