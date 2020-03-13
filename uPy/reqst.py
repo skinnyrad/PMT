906bcc71fd0d21513e91fdc7cee80b41b3543bbd
comptonben <comptonben504@gmail.com>
2020-03-13 16:16:44 -0500

    quick fix

--- uPy/reqst.py
+++ uPy/reqst.py
@@ -50,7 +50,7 @@ def request_dns_internet(method, url, data=None, json=None, headers={}, stream=N
         s.connect(ai[-1])
         if proto == "https:":
             s = ussl.wrap_socket(s, server_hostname=host)
-        s.write(b"%s / HTTP/1.0\r\n" % (method)
+        s.write(b"%s / HTTP/1.0\r\n" % (method))
         if not "Host" in headers:
             s.write(b"Host: %s\r\n" % host)
         # Iterate over keys to avoid tuple alloc
@@ -94,7 +94,7 @@ def request_dns_internet(method, url, data=None, json=None, headers={}, stream=N
                 gc.collect()
                 print("Redirection [{}]".format(location))
                 # need to get the method from the redirection
-                return [status,location]
+                return [status,location,None]
     except OSError as err:
         # if not s:
         #     s.close()
