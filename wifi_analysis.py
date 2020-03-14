351ba467b00d7a3f67417e7fdc8a0fe18020a7cb
comptonben <comptonben504@gmail.com>
2020-03-14 18:56:41 -0500

    let the one liners begin

--- wifi_analysis.py
+++ wifi_analysis.py
@@ -16,13 +16,20 @@ def main():
     r = re.compile(r"^SSID: ([\w\W])+ Connected, POST: (True|False)$")
     r_true = re.compile(r"([\w\W])+(True)$")
 
-    for line in contents:
-        if r.match(line):
-            matches.append(line)
+    """
+    " the following for loops are represented by the one liners below them
+    """
+    # for line in contents:
+    #     if r.match(line):
+    #         matches.append(line)
 
-    for match in matches:
-        if r_true.match(match):
-            trues.append(match)
+    # for match in matches:
+    #     if r_true.match(match):
+    #         trues.append(match)
+
+    list(matches.append(line) if r.match(line) else None for line in contents)
+
+    list(trues.append(match) if r_true.match(match) else None for match in matches)
 
     print("WiFi Connection Analysis:\n|")
     print(f"| --> Number of Posts Attemped - {len(matches)}")
