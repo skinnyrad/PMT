5b773c0246efbb47fb2911d29a0b76b21be21903
David <dmtougaw@gmail.com>
2020-03-27 14:34:43 -0500

    PHP null request fix, map plots plaintext data, config init

--- Javascript/map/app.js
+++ Javascript/map/app.js
@@ -6,7 +6,9 @@ var mapLat = 34.724600;
 var mapLng = -86.639590;
 var mapDefaultZoom = 5;
 var latLonDecimals = 7;
-var url = "https://pmtlogger.000webhostapp.com/api/getJSON.php";
+
+// var url = "https://pmtlogger.000webhostapp.com/api/getJSON.php";
+var host_conf = "None";
 var url_encryption = "https://pmtlogger.000webhostapp.com/api/getEnc.php";
 var data = null;
 
@@ -225,8 +227,10 @@ function add_data_points(data,color) {
     var id = 1;
     data.forEach(d => {
         $('#table-data').append("<tr onclick='zoomTo("+d.Latitude+","+d.Longitude+");'><td>"+id+"</td><td>"+
-            d.Latitude.toFixed(latLonDecimals)+"</td><td>"+
-            d.Longitude.toFixed(latLonDecimals)+"</td><td>"+
+            // d.Latitude.toFixed(latLonDecimals)+"</td><td>"+
+            // d.Longitude.toFixed(latLonDecimals)+"</td><td>"+
+            d.Latitude+"</td><td>"+
+            d.Longitude+"</td><td>"+
             d.Date+" "+
             d.Time+"</td></tr>");
         d.id = id++;
@@ -362,6 +366,24 @@ function initialize_map() {
     });
 
     initComponents();
+    var host = prompt("Please enter your HOST","https://pmtlogger.000webhostapp.com");
+    var url = host+"/api/getJSON.php";
+    host_conf = url;
+
+    // GET data points
+    $.ajax(
+        {
+            url: url,
+            //context: document.body
+            success: function(result) {
+                let inCSV = JSON.parse(result);
+                add_data_points(inCSV,'red');
+            },
+            error: function (err) {
+                console.alert(err);
+            }
+        });
+    
 }
 
 function add_map_point(props, color) {
